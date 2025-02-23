3acd14fe40c1805e084c1fce3c1a33f2af1d8600
kingdomhearts2 <vraimentmalheureux@gmail.com>
2025-02-23 18:45:03 +0100

    ouais

--- push_swap.c
+++ push_swap.c
@@ -1,16 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: silent <silent@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/23 18:41:47 by silent            #+#    #+#             */
+/*   Updated: 2025/02/23 18:42:25 by silent           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	t_stack_node	*a;
 	t_stack_node	*b;
 
 	a = NULL;
 	b = NULL;
-	if (argc == 1 || (argc == 2 && !argv[1][0]))
-	return (1);
-if (argc == 2)
-	argv = ft_split(argv[1], ' ');
-else
-	argv = argv + 1;
+	if (argc <= 1 || (argc == 2 && !argv[1][0]))
+		return (1);
+	if (argc == 2)
+		argv = ft_split(argv[1], ' ');
+	else
+		argv = argv + 1;
+	init_stack_a(&a, argv);
+	if (!stack_sorted(a))
+	{
+		if (stack_len(a) == 2)
+			sa(&a);
+		else if (stack_len(a) == 3)
+			sort_three(&a);
+		else
+			sort_stacks(&a, &b);
+	}
+	free_stack(&a);
+	if (argc == 2)
+		free(argv - 1);
+	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -1,22 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: silent <silent@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/23 18:42:39 by silent            #+#    #+#             */
+/*   Updated: 2025/02/23 18:44:41 by silent           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
 # include <limits.h>
 # include "./inc/libft/libft.h"
 
-
 typedef struct s_stack_node
 {
-	int	nbr;
-	int	index;
-	int	push_cost;
-	int	above_median;
-	int	cheapest;
+	int					nbr;
+	int					index;
+	int					push_cost;
+	int					above_median;
+	int					cheapest;
 
 	struct s_stack_node	*target_node;
 	struct s_stack_node	*next;
-	struct s_stack_node *prev;
-} t_stack_node;
-
+	struct s_stack_node	*prev;
+}	t_stack_node;
 
 #endif
