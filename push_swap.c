a3cb454ed86b2ecea1df77341c8d6c3f92b9a4c6
laptop <silenthill@silenthill.>
2025-02-23 17:44:00 +0100

    ps

---
+++ push_swap.c
@@ -0,0 +1,16 @@
+#include "push_swap.h"
+
+int main(int argc, char **argv)
+{
+	t_stack_node	*a;
+	t_stack_node	*b;
+
+	a = NULL;
+	b = NULL;
+	if (argc == 1 || (argc == 2 && !argv[1][0]))
+	return (1);
+if (argc == 2)
+	argv = ft_split(argv[1], ' ');
+else
+	argv = argv + 1;
+}
