63e2790ecc3e8c1924e91e8a2c6fbc2911ee9168
kingdomhearts2 <vraimentmalheureux@gmail.com>
2025-02-24 19:49:57 +0100

    swap done

--- swap.c
+++ swap.c
@@ -6,7 +6,32 @@
 /*   By: edjebri <edjebri@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 14:53:22 by edjebri           #+#    #+#             */
-/*   Updated: 2025/02/24 14:53:23 by edjebri          ###   ########.fr       */
+/*   Updated: 2025/02/24 19:43:49 by edjebri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+void	swap(t_stack_node **stack)
+{
+	t_stack_node	*first_node;
+	t_stack_node	*second_node;
+
+	if (*stack == NULL || stack == NULL || (*stack)->next == NULL)
+		return ;
+	first_node = *stack;
+	second_node = first_node->next;
+	if (second_node->next != NULL)
+		second_node->next->prev = *stack;
+	first_node->prev = second_node;
+	first_node->next = second_node->next;
+	second_node->next = first_node;
+	second_node->prev = NULL;
+	*stack = second_node;
+}
+
+void	sa(t_stack_node **a)
+{
+	swap(a);
+	fftprintf()
+}
